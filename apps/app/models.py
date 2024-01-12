26a84f4b5ce64d8cb5058f5addde85753de8b01a
Fernando Pacheco <131721500+fernando-pacheco@users.noreply.github.com>
2024-01-12 01:28:46 -0300

    Update models.py

--- apps/app/models.py
+++ apps/app/models.py
@@ -1,12 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
-# models.py
-
-from django.db import models
-from django.contrib.auth.models import User
-
 class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
